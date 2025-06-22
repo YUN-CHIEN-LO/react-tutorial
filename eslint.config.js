36ce43637bd1803dba4231cda7e0b86bf60e309d
YUN-CHIEN-LO <yunchienlo@gmail.com>
2025-06-23 00:06:25 +0800

    feat: 配置 lint & format

--- eslint.config.js
+++ eslint.config.js
@@ -1,28 +1,47 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
+// eslint.config.js
+import js from '@eslint/js';
+import globals from 'globals';
+import react from 'eslint-plugin-react';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import prettier from 'eslint-plugin-prettier';
+import tseslint from 'typescript-eslint';
+import parser from '@typescript-eslint/parser';
 
 export default tseslint.config(
-  { ignores: ['dist'] },
+  { ignores: ['dist', 'node_modules'] },
   {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended],
+    name: 'base',
     files: ['**/*.{ts,tsx}'],
     languageOptions: {
+      parser,
       ecmaVersion: 2020,
-      globals: globals.browser,
+      sourceType: 'module',
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+      },
     },
     plugins: {
+      react,
       'react-hooks': reactHooks,
       'react-refresh': reactRefresh,
+      prettier,
+    },
+    settings: {
+      react: {
+        version: 'detect',
+      },
     },
     rules: {
+      ...js.configs.recommended.rules,
+      ...react.configs.recommended.rules,
       ...reactHooks.configs.recommended.rules,
-      'react-refresh/only-export-components': [
-        'warn',
-        { allowConstantExport: true },
-      ],
+
+      'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+
+      'prettier/prettier': 'error', // run prettier as lint rule
+      'react/react-in-jsx-scope': 'off',
     },
   },
-)
+);
